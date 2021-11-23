e0657e966bb359c9af5451f578d30eae60abdc63
LakshitXD <lakshitk6666@gmail.com>
2021-11-23 20:01:54 +0530

    devise-jwt setup

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  devise :database_authenticatable,
+         :jwt_authenticatable,
+         :registerable,
+         jwt_revocation_strategy: JwtDenylist
+end
