33bdb5022139fc014799cd44ae9b2c432f5bb308
LakshitXD <lakshitk6666@gmail.com>
2021-11-25 11:33:23 +0530

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :users,
-             controllers: {
-               sessions: 'users/sessions',
-               registrations: 'users/registrations'
-             }
+  devise_for :users, path: 'api/v1/users',
+                     controllers: {
+                       sessions: 'api/v1/users/sessions',
+                       registrations: 'api/v1/users/registrations'
+                     }
   get '/member-data', to: 'members#show'
 end
